e72a067d606bc4177b97cb3158c29cd8fafbf3d4
vad-naitik <vad.naitik@gmail.com>
2022-09-29 12:19:39 +0530

    update

--- index.php
+++ index.php
@@ -1,6 +1,3 @@
 <?php
 
-require_once "vendor/autoload.php";
-
-// $class = new \VadNaitik\WowzaStreaming\StreamingServices();
-// echo $class->echoPhrase("It's working");
+	require_once "vendor/autoload.php";
