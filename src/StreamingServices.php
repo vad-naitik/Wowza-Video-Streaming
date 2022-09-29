57b041f95ae08d4ba9d364c17929636bd0878625
vad-naitik <vad.naitik@gmail.com>
2022-09-29 14:39:56 +0530

    update

--- src/StreamingServices.php
+++ src/StreamingServices.php
@@ -4,22 +4,49 @@ namespace VadNaitik\WowzaStreaming;
 
 class StreamingServices {
 
-    /**
-     * Create a new Skeleton Instance
-     */
-    public function __construct()
-    {
-    }
+	/** @var string The Stripe API key to be used for requests. */
+    public static $apiKey;
 
-    /**
-     * Friendly welcome
-     *
-     * @param string $phrase Phrase to return
-     *
-     * @return string Returns the phrase passed in
-     */
-    public static function echoPhrase($phrase)
-    {
-        return $phrase;
-    }
+    /** @var string The Stripe client_id to be used for Connect requests. */
+    public static $accessKey;
+
+    /** @var string The base URL for the Stripe API. */
+    public static $apiBase = 'https://api.video.wowza.com/api/v1.8';
+
+	/**
+	 * Create a new Skeleton Instance
+	 */
+	public function __construct() {
+
+		$this->header	= [
+			"Content-Type:"  	. "application/json",
+			"charset:"			. "utf-8",
+			"wsc-api-key:"		. "",
+			"wsc-access-key:"	. "",
+		];
+
+	}
+
+	/**
+	 * Friendly welcome
+	 *
+	 * @param string $phrase Phrase to return
+	 *
+	 * @return string Returns the phrase passed in
+	 */
+	public static function echoPhrase($phrase) {
+		return $apiKey . " = " . $accessKey ;
+	}
+
+/* =
+/* ============================================================================ */
+	public static function setApiKey($apiKey) {
+		self::$apiKey = $apiKey;
+	}
+
+	public static function setAccessKey($accessKey) {
+		self::$accessKey = $accessKey;
+	}
+
+
 }
